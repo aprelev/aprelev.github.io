e40a84a304d8f13b160f0c38261322c9a0017191
Thomas Aprelev <thomas@anjuna.io>
2020-05-19 23:06:06 -0700

    Self-hosts JQuery and FontAwesome

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -2,14 +2,12 @@ $(document).ready(function() {
   $('.tabs').each(function(index) {
     var $tabParent = $(this);
     var $tabs = $tabParent.find('li');
-    var $contents = $tabParent.parents().next('.tabs-content').find('.tab-content');
-
+    var $contents =
+        $tabParent.parents().next('.tabs-content').find('.tab-content');
     $tabs.click(function() {
       var curIndex = $(this).index();
-      // toggle tabs
       $tabs.removeClass('is-active');
       $tabs.eq(curIndex).addClass('is-active');
-      // toggle contents
       $contents.removeClass('is-active');
       $contents.eq(curIndex).addClass('is-active');
     });
