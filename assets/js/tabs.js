f1edaf8f004a66a80c97fc3c2d4549f02dad0231
Thomas Aprelev <thomas@anjuna.io>
2020-05-25 19:32:33 -0700

    Implements first major overhaul of theme and content

---
+++ assets/js/tabs.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.tabs').each(function(index) {
+    var $tabParent = $(this);
+    var $tabs = $tabParent.find('li');
+    var $contents =
+        $tabParent.next('.tabs-content').find('.tab-content');
+    $tabs.click(function() {
+      var curIndex = $(this).index();
+      $tabs.removeClass('is-active');
+      $tabs.eq(curIndex).addClass('is-active');
+      $contents.removeClass('is-active');
+      $contents.eq(curIndex).addClass('is-active');
+    });
+  });
+});
